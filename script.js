9047656b63c729af0b6e2d5af1002c98b293521b
Joakim Kollerud <j_kollerud@hotmail.com>
2025-06-04 11:06:39 +0200

    project cards, contact me cta, links, img screenshot

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const profileImage = document.getElementById("profileImage");
+	const contactButton = document.getElementById("contactButton");
 	const bioBox = document.getElementById("bioBox");
+	const contactBox = document.getElementById("contactBox");
 	const ingressContainer = document.getElementById("ingressContainer");
 	const ingressHeader = document.querySelector(".ingress_header");
 
-	profileImage.addEventListener("click", () => {
-		const isVisible = bioBox.classList.contains("show");
-
-		if (isVisible) {
-			bioBox.classList.remove("show");
+	function fadeToBox(fromBox, toBox) {
+		if (fromBox && fromBox.classList.contains("show")) {
+			fromBox.classList.remove("show");
 			ingressContainer.classList.remove("slide-up");
 			ingressHeader.classList.remove("expanded");
 
 			setTimeout(() => {
-				bioBox.classList.add("hidden");
+				fromBox.style.display = "none";
+
+				toBox.style.display = "block";
+				setTimeout(() => {
+					toBox.classList.add("show");
+					ingressContainer.classList.add("slide-up");
+					ingressHeader.classList.add("expanded");
+				}, 10);
 			}, 400);
 		} else {
-			bioBox.classList.remove("hidden");
-
+			toBox.style.display = "block";
 			setTimeout(() => {
-				bioBox.classList.add("show");
+				toBox.classList.add("show");
 				ingressContainer.classList.add("slide-up");
 				ingressHeader.classList.add("expanded");
 			}, 10);
+		}
+	}
+
+	function closeBox(box) {
+		box.classList.remove("show");
+		ingressContainer.classList.remove("slide-up");
+		ingressHeader.classList.remove("expanded");
+
+		setTimeout(() => {
+			box.style.display = "none";
+		}, 400);
+	}
+
+	profileImage.addEventListener("click", () => {
+		fadeToBox(contactBox, bioBox);
+	});
+
+	contactButton.addEventListener("click", () => {
+		fadeToBox(bioBox, contactBox);
+	});
+
+	document.addEventListener("click", (e) => {
+		const insideElements = [profileImage, bioBox, contactButton, contactBox];
+		const clickedInside = insideElements.some((el) => el.contains(e.target));
+
+		if (!clickedInside) {
+			[bioBox, contactBox].forEach((box) => {
+				if (box.classList.contains("show")) {
+					closeBox(box);
+				}
+			});
 		}
 	});
 });
